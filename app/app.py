f08a6d3ee025c2972ace84b796d7977db80725a8
liorkrispin <79877903+liorkrispin@users.noreply.github.com>
2025-01-27 15:29:16 +0200

    Update app.py
    change to port 5001

--- app/app.py
+++ app/app.py
@@ -378,4 +378,4 @@ def ping():
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
